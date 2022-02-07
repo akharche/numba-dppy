2fe052b8b343f1b171b6ef25896e62ae765a1d0c
akharche <angelina.kharchevnikova@intel.com>
2022-02-07 16:50:56 +0300

    test

--- numba_dppy/examples/atomic_op.py
+++ numba_dppy/examples/atomic_op.py
@@ -45,6 +45,7 @@ def main():
 
     # Use the environment variable SYCL_DEVICE_FILTER to change the default device.
     # See https://github.com/intel/llvm/blob/sycl/sycl/doc/EnvironmentVariables.md#sycl_device_filter.
+
     device = dpctl.select_default_device()
     print("Using device ...")
     device.print_device_info()
